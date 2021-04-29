c22d725297f8a33002b147c49a6417f2a61b0610
Patrick Robinson <patrickrobinson@MacBook-Pro-de-Patrick-2.local>
2021-04-28 23:54:42 -0600

    clearing repo

--- PokeApp/app/src/main/java/com/proyecto/pokeapp/extensions/BooleanExtensions.kt
+++
@@ -1,5 +0,0 @@
-package com.proyecto.pokeapp.extensions
-
-import android.view.View
-
-fun Boolean.mapToVisibility() : Int = if (this) View.VISIBLE else View.GONE
--- PokeApp/app/src/main/java/com/proyecto/pokeapp/models/pokemon.kt
+++
@@ -1,3 +0,0 @@
-package com.proyecto.pokeapp.models
-
-data class Pokemon(val name: String, val imageURL: String, val type: String)
