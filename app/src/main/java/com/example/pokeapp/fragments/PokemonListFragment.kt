9a084434d0e4a98b2ad901c93fb49fef7130e756
patrickrobinson <patrickrobinsonx@gmail.com>
2021-05-18 09:44:12 -0600

    Search capability

--- app/src/main/java/com/example/pokeapp/fragments/PokemonListFragment.kt
+++ app/src/main/java/com/example/pokeapp/fragments/PokemonListFragment.kt
@@ -10,17 +10,26 @@ import com.example.pokeapp.R
 import com.example.pokeapp.adapters.PokemonAdapter
 import com.example.pokeapp.databinding.FragmentPokemonListBinding
 import com.example.pokeapp.viewmodels.PokemonListViewModel
+import com.jakewharton.rxbinding4.widget.textChanges
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.fragment_pokemon_list.view.*
+import java.util.concurrent.TimeUnit
+
 
 class PokemonListFragment : Fragment(R.layout.fragment_pokemon_list) {
 
     private var _binding: FragmentPokemonListBinding? = null
     private val binding: FragmentPokemonListBinding get() = _binding!!
 
+    private val disposable = CompositeDisposable()
+
     private val viewModel: PokemonListViewModel by viewModels()
     private val adapter = PokemonAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         // make the call to the server
         viewModel.makeAPIRequest()
@@ -38,10 +47,15 @@ class PokemonListFragment : Fragment(R.layout.fragment_pokemon_list) {
 
     override fun onDestroyView() {
         super.onDestroyView()
-
+        disposable.clear()
         _binding = null
     }
 
+    fun Fragment?.runOnUiThread(action: () -> Unit) {
+        this ?: return
+        if (!isAdded) return // Fragment not attached to an Activity
+        activity?.runOnUiThread(action)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -52,5 +66,37 @@ class PokemonListFragment : Fragment(R.layout.fragment_pokemon_list) {
         viewModel.getPokemonList().observe(viewLifecycleOwner) {
             adapter.pokemons = it
         }
+
+        Thread(Runnable {
+            disposable.add(
+                binding.searchBox.textChanges()
+                    .skipInitialValue()
+                    .debounce(300, TimeUnit.MILLISECONDS)
+                    .map { it.toString() }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        if (it.isEmpty()) {
+                            viewModel.makeAPIRequest()
+
+                        } else {
+                            //adapter.pokemons = viewModel.searchPokemon(binding.textInputLayoutSearch.searchBox.text.toString()).value!!
+                            //viewModel.findPokemon(binding.textInputLayoutSearch.searchBox.text.toString())
+                           //runOnUiThread{
+                               viewModel.makeAPIRequest2(binding.textInputLayoutSearch.searchBox.text.toString())
+                              //  val poke = PokemonResult("test","https://pokeapi.co/api/v2/pokemon/25/")
+                              //  val testList: List<PokemonResult> = listOf(poke)
+                              //  adapter.pokemons = testList
+                            // }
+                        }
+                    }
+            )
+
+        }).start()
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/pokeapp/viewmodels/PokemonListViewModel.kt
+++ app/src/main/java/com/example/pokeapp/viewmodels/PokemonListViewModel.kt
@@ -13,10 +13,12 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 class PokemonListViewModel : ViewModel() {
 
     private val pokemonList = MutableLiveData<List<PokemonResult>>()
     private var pokeApiService: IPokeAPIService
+    private val pokemonfilteredList = MutableLiveData<List<PokemonResult>>()
 
     init {
         val retrofit = Retrofit.Builder()
@@ -25,6 +27,7 @@ class PokemonListViewModel : ViewModel() {
             .build()
 
         pokeApiService = retrofit.create(IPokeAPIService::class.java)
+
     }
 
     fun makeAPIRequest() {
@@ -46,7 +49,39 @@ class PokemonListViewModel : ViewModel() {
             })
     }
 
+    fun makeAPIRequest2(keyword: String) {
+        var res:List<PokemonResult> = emptyList()
+        pokeApiService.getPokemonList(Constants.TOTAL_POKEMONS)
+            .enqueue(object: Callback<PokemonResponse>{
+                override fun onResponse(
+                    call: Call<PokemonResponse>,
+                    response: Response<PokemonResponse>
+                ) {
+                    response.body()?.let {
+                        // notify to the subscribers
+                       // pokemonList.setValue(listOf(it.results.find { it.name == "pikachu" } ) as List<PokemonResult>?)
+                        res =listOf(it.results.find { it.name == keyword } ) as List<PokemonResult>
+                        if (res[0] != null) {
+                            pokemonList.setValue(res)
+                        }
+
+                    }
+                }
+
+                override fun onFailure(call: Call<PokemonResponse>, t: Throwable) {
+                }
+            })
+    }
+
+
     fun getPokemonList(): LiveData<List<PokemonResult>> {
         return pokemonList
     }
+
+    fun searchPokemon(keyword:String): LiveData<List<PokemonResult>> {
+        return pokemonList
+    }
+
+
+
 }
